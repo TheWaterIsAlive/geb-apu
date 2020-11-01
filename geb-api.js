dad8c86a9db415d59641c8c82ffe09ba488e1d58
unknown <ajfry203@gmail.com>
2020-11-01 18:34:23 +1300

    init setup

---
+++ geb-api.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+let items = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
